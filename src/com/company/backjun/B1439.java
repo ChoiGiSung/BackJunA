bc4d6ea23f4b8c6176598e3c0e48b71b1af84145
gisungchoi <chlrltjd159@naver.com>
2021-02-14 21:46:00 +0900

    'B1439'

---
+++ src/com/company/backjun/B1439.java
@@ -0,0 +1,48 @@
+package com.company.backjun;
+
+
+import java.util.Scanner;
+
+import java.util.*;
+public class B1439 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        String input = sc.nextLine();
+        boolean flag;
+        int zeroCount=0;
+        int oneCount=0;
+        char[] getChar = input.toCharArray();
+        if (input.length()==0){
+            System.out.println(0);
+            return;
+        }
+
+
+        flag = input.startsWith("0")? false:true;
+        if (!flag){
+            zeroCount++;
+        }else {
+            oneCount++;
+        }
+
+        char nowChar=getChar[0];
+
+        for (int i = 1; i < getChar.length; i++) {
+            if(nowChar != getChar[i]){
+                if(flag){
+                    zeroCount++;
+                    flag=false;
+                    nowChar=getChar[i];
+                }else {
+                    oneCount++;
+                    flag=true;
+                    nowChar=getChar[i];
+                }
+            }
+        }
+
+        System.out.println(Math.min(zeroCount,oneCount));
+
+    }
+}
