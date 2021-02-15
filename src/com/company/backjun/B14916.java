053078c4f8434fcfca8452d6d54650880e0e6bed
gisungchoi <chlrltjd159@naver.com>
2021-02-15 23:04:09 +0900

    'B14916'

---
+++ src/com/company/backjun/B14916.java
@@ -0,0 +1,31 @@
+package com.company.backjun;
+
+import java.util.*;
+public class B14916 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int All=sc.nextInt();
+
+        int count=0;
+        while (All>0){
+            if (All % 5 ==0){
+                count+=All/5;
+                break;
+            }else if(All % 2 == 0){
+                All-=2;
+                count++;
+            }else if(All > 5){
+                All-=5;
+                count++;
+            }else {
+                count=-1;
+                break;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
