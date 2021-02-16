e33a28c8eeb7651a5931831b34089b801debb088
gisungchoi <chlrltjd159@naver.com>
2021-02-16 22:39:17 +0900

    B2217
    쉽게 푼 실버 4 문제

---
+++ src/com/company/backjun/B2217.java
@@ -0,0 +1,28 @@
+package com.company.backjun;
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import java.util.*;
+public class B2217 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        ArrayList<Integer>num=new ArrayList<>();
+        int allInt = sc.nextInt();
+
+        for (int i = 0; i < allInt; i++) {
+            num.add(sc.nextInt());
+        }
+
+        Collections.sort(num);
+
+        List<Integer> weight=new ArrayList<>();
+        for (int i = 0; i < num.size(); i++) {
+            weight.add(num.get(i)*(num.size()-i));
+        }
+
+        System.out.println(Collections.max(weight));
+    }
+}
