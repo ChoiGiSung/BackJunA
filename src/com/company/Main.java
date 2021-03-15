a0f6fc6f4e2fab5bf2abc3f62044fc1aa6e39bff
gisungchoi <chlrltjd159@naver.com>
2021-03-15 21:55:43 +0900

    B13305

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,21 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        boolean isUser=true;
-        boolean isAdult=true;
-        boolean isFaint=true;
+//        boolean isUser=true;
+//        boolean isAdult=true;
+//        boolean isFaint=true;
+//
+//        makeMember(isAdult ? 15:20, isFaint ? 100:1_000_000, isUser ? "일반유저":"관리자");
 
-        makeMember(isAdult ? 15:20, isFaint ? 100:1_000_000, isUser ? "일반유저":"관리자");
-
+        String a=null;
+        System.out.println(a.isEmpty());
     }
 
 
     public static void makeMember(int age,int money,String position){
         System.out.println( "당신은 "+position+" 나이는 : "+age+"잔고는 : "+money);
     }
+
+
 }
 
