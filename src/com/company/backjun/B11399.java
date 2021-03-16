e3e9a3dbe2dcc1fb581d2b4eedc54c698352f9d8
gisungchoi <chlrltjd159@naver.com>
2021-03-16 12:09:01 +0900

    B11399

---
+++ src/com/company/backjun/B11399.java
@@ -0,0 +1,33 @@
+package com.company.backjun;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+
+import java.util.*;
+public class B11399 {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        int allPerson = sc.nextInt();
+        List<Integer> allTime =new ArrayList<>();
+
+        int count = 0;
+        for (int i = 0; i < allPerson; i++) {
+            allTime.add(sc.nextInt());
+
+        }
+        Collections.sort(allTime);
+
+        for (int i = 0; i < allTime.size(); i++) {
+            for (int j = 0; j <= i; j++) {
+                count+=allTime.get(j);
+            }
+
+        }
+        System.out.println(count);
+    }
+}
