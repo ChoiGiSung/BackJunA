34291130d364e585f17bd49eb30a060bb348c9a0
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2021-09-28 22:53:07 +0900

    refactor : 자바 버전에 따른 주석 처리

--- test/com/company/programmers/KeypadTest.java
+++ test/com/company/programmers/KeypadTest.java
@@ -1,20 +1,20 @@
-package com.company.programmers;
-
-import com.company.programmers.normal.Keypad;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-class KeypadTest {
-
-    Keypad keypad=new Keypad();
-
-    @Test
-    public void 키패드(){
-        Keypad.Solution solution= keypad.new Solution();
-
-        int []number={0, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
-        String hand="right";
-
-        Assertions.assertEquals("RLRLLRLLRL",solution.solution(new int[]{2, 2, 3, 4, 5, 6, 7, 8, 9, 0},"left"));
-    }
-}
+//package com.company.programmers;
+//
+//import com.company.programmers.normal.Keypad;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//
+//class KeypadTest {
+//
+//    Keypad keypad=new Keypad();
+//
+//    @Test
+//    public void 키패드(){
+//        Keypad.Solution solution= keypad.new Solution();
+//
+//        int []number={0, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+//        String hand="right";
+//
+//        Assertions.assertEquals("RLRLLRLLRL",solution.solution(new int[]{2, 2, 3, 4, 5, 6, 7, 8, 9, 0},"left"));
+//    }
+//}
