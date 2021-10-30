6bdc29cc911c5936f9e70ae50ae48bc398f272c4
gisungchoi <chlrltjd159@naver.com>
2021-10-30 18:46:49 +0900

    feat : 잘못된 코드

---
+++ src/com/company/programmers/runing/Wrong1.java
@@ -0,0 +1,36 @@
+package com.company.programmers.runing;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Wrong1 {
+    import java.util.*;
+
+    class Solution {
+        public String solution(int[] numbers) {
+            String answer = "";
+            int length = numbers.length;
+            String[] str = new String[length];
+
+            for (int i = 0; i < length; i++) {
+                str[i] = String.valueOf(numbers[i]);
+            }
+
+            Arrays.sort(str, new Comparator<String>() {
+                public int compare(String a, String b) {
+                    return (b+a).compareTo(a + b);
+                }
+            });
+
+            for (int i = 0; i < str.length; i++) {
+                answer += str[i];
+            }
+
+            if(answer.startsWith("0")){
+                return "0";
+            }
+
+            return answer;
+        }
+    }
+}
