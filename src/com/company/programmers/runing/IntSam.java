a301f863206da4a66004784c19b60eb9a4bd38c9
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2021-11-09 21:12:24 +0900

    feat : dp
    - 역순으로

--- src/com/company/programmers/runing/IntSam.java
+++ src/com/company/programmers/runing/IntSam.java
@@ -5,27 +5,18 @@ public class IntSam {
     class Solution {
         public int solution(int[][] triangle) {
             int answer = 0;
-            for (int i = 1; i < triangle.length; i++) {
+
+            for (int i = triangle.length - 2; i >= 0; i--) {
                 for (int j = 0; j < triangle[i].length; j++) {
+                    int left = triangle[i+1][j];
+                    int right = triangle[i+1][j+1];
 
-                    if(j == 0){
-                        triangle[i][j] += triangle[i - 1][j];
-                    }else if(j == triangle[i].length - 1){
-                        triangle[i][j] += triangle[i - 1][j - 1];
-                    }else {
-                        int left = triangle[i - 1][j - 1];
-                        int right = triangle[i - 1][j];
-
-                        triangle[i][j] += Math.max(left,right);
-                    }
-
-                    answer = Math.max(answer,triangle[i][j]);
+                    triangle[i][j] += Math.max(left,right);
                 }
 
             }
 
-
-            return answer;
+            return triangle[0][0];
         }
     }
 }
